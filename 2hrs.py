69f37bb9a7c0411a7bcfe4733e9c4a134feef7b5
Sergiy Shpak <g705586@ad.syniverse.com>
2019-07-31 15:59:15 -0400

    delve kills for last 2 hours

---
+++ 2hrs.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 14:40:40 2019
+
+@author: g705586
+
+
+https://esi.evetech.net/v1/killmails/75962440/2195704dfa6fac7ad4b27852a16d9cd311aaf59c/
+
+https://zkillboard.com/api/kills/regionID/10000060/pastSeconds/7200/
+
+50010993   Stargate (T5ZI-S)
+50010996   Stargate (MO-GZ5)
+50010664  Stargate (E3OI-U)
+
+         50010993 TotalVal   389116364.13  | ship Cynabal                    | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78142460/
+         50010589 TotalVal    54649768.88  | ship Stiletto                   | sysa               E3OI-U | dist from sun           3.66 |ZKB         https://zkillboard.com/kill/78142367/
+         50013771 TotalVal      880323.60  | ship Velator                    | sysa               A-ELE2 | dist from sun          15.58 |ZKB         https://zkillboard.com/kill/78142352/
+         50010993 TotalVal       11286.99  | ship Impairor                   | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78142347/
+         40301374 TotalVal   236045055.73  | ship Gila                       | sysa               1DQ1-A | dist from sun           0.90 |ZKB         https://zkillboard.com/kill/78142309/
+         50012327 TotalVal   118298103.39  | ship Noctis                     | sysa               MO-GZ5 | dist from sun          10.90 |ZKB         https://zkillboard.com/kill/78142265/
+         50011100 TotalVal    59822371.23  | ship Myrmidon                   | sysa               LUA5-L | dist from sun          39.53 |ZKB         https://zkillboard.com/kill/78142244/
+         50010993 TotalVal    44144856.44  | ship Caracal                    | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78142211/
+         40301370 TotalVal    47665310.93  | ship Vexor                      | sysa               1DQ1-A | dist from sun           0.33 |ZKB         https://zkillboard.com/kill/78142096/
+         50010993 TotalVal   885954006.40  | ship Loki                       | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78142029/
+         50010993 TotalVal   557530890.64  | ship Tengu                      | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141969/
+         50010993 TotalVal    13853233.98  | ship Raptor                     | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141866/
+         40298278 TotalVal    75479457.85  | ship Myrmidon                   | sysa               NOL-M9 | dist from sun           3.23 |ZKB         https://zkillboard.com/kill/78141861/
+         40301733 TotalVal    46643838.63  | ship Hound                      | sysa               3-DMQT | dist from sun           5.45 |ZKB         https://zkillboard.com/kill/78141814/
+         50010993 TotalVal    55579588.27  | ship Hound                      | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141755/
+         50010993 TotalVal   819883089.15  | ship Loki                       | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141752/
+         50010993 TotalVal    61504989.25  | ship Gnosis                     | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141700/
+         50010993 TotalVal   220306756.55  | ship Crane                      | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141681/
+         40301378 TotalVal     4071967.86  | ship Epithal                    | sysa               1DQ1-A | dist from sun           1.84 |ZKB         https://zkillboard.com/kill/78141636/
+         50010664 TotalVal    50841332.36  | ship Stiletto                   | sysa               R5-MM8 | dist from sun           8.46 |ZKB         https://zkillboard.com/kill/78141548/
+         50010664 TotalVal    43026118.12  | ship Ishkur                     | sysa               R5-MM8 | dist from sun           8.46 |ZKB         https://zkillboard.com/kill/78141543/
+         50010996 TotalVal    58376274.63  | ship Harpy                      | sysa               1DQ1-A | dist from sun          43.02 |ZKB         https://zkillboard.com/kill/78141479/
+         40298385 TotalVal     1308471.81  | ship Impairor                   | sysa               O-IOAI | dist from sun          60.59 |ZKB         https://zkillboard.com/kill/78141362/
+         40301542 TotalVal  2596755529.94  | ship Paladin                    | sysa               5BTK-M | dist from sun           9.48 |ZKB         https://zkillboard.com/kill/78141244/
+         40298325 TotalVal    69915923.50  | ship Myrmidon                   | sysa               NOL-M9 | dist from sun          38.57 |ZKB         https://zkillboard.com/kill/78141183/
+         50010996 TotalVal     1819326.02  | ship Heron                      | sysa               1DQ1-A | dist from sun          43.02 |ZKB         https://zkillboard.com/kill/78141179/
+         50010993 TotalVal    66094264.84  | ship Manticore                  | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141076/
+         50010993 TotalVal   167346049.33  | ship Phantasm                   | sysa               1DQ1-A | dist from sun          47.33 |ZKB         https://zkillboard.com/kill/78141030/
+         50010535 TotalVal    17992436.56  | ship Magnate                    | sysa               RF-K9W | dist from sun           4.39 |ZKB         https://zkillboard.com/kill/78141027/
+         40299753 TotalVal   310476951.08  | ship Armageddon                 | sysa               T5ZI-S | dist from sun           4.21 |ZKB         https://zkillboard.com/kill/78140983/
+         40299753 TotalVal    10775557.87  | ship Mobile Tractor Unit        | sysa               T5ZI-S | dist from sun           4.21 |ZKB         https://zkillboard.com/kill/78140980/
+         50010996 TotalVal   314364146.93  | ship Eagle                      | sysa               1DQ1-A | dist from sun          43.02 |ZKB         https://zkillboard.com/kill/78140881/
+         40300934 TotalVal    10585230.40  | ship Algos                      | sysa               QY6-RK | dist from sun           3.74 |ZKB         https://zkillboard.com/kill/78140873/
+         40300934 TotalVal    28763600.59  | ship Caracal                    | sysa               QY6-RK | dist from sun           3.74 |ZKB         https://zkillboard.com/kill/78140853/
+         50010996 TotalVal    23334401.03  | ship Imperial Navy Slicer       | sysa               1DQ1-A | dist from sun          43.02 |ZKB         https://zkillboard.com/kill/78140842/
+         40299104 TotalVal    60706329.72  | ship Myrmidon                   | sysa               E3OI-U | dist from sun           2.68 |ZKB         https://zkillboard.com/kill/78140752/
+
+"""
+
+            
+import requests
+import json
+import time 
+import sys
+import math
+
+
+systemDict= {} 
+typesDict=  {}
+locDict=  {}
+systemSec= {} 
+
+
+
+sleepTime=3
+
+sys_sys={}
+with open("sys_sys_topo1.txt", "r") as ins:
+    for line in ins:
+        line = line[:-1]
+        my_list = line.split(",")
+        sys_sys[my_list[0]] = my_list[1]
+
+with open("mapSolarSystems100.csv", "r") as ins:
+    for line in ins:
+        my_list = line.split(",")
+        #print(my_list[2]+"   " +my_list[3])
+        systemDict[my_list[2]] = my_list[3]
+        systemSec[my_list[2]] = my_list[4]
+
+with open("invTypes.csv", "r") as ins:
+    for line in ins:
+        my_list = line.split(",")
+        typesDict[my_list[0]] = my_list[2]
+
+with open("loc.csv", "r") as ins:
+    for line in ins:
+        line = line[:-1]
+        my_list = line.split(",")
+        locDict[my_list[0]] = my_list[1]
+
+
+url = 'https://zkillboard.com/api/kills/regionID/10000060/pastSeconds/7200/' #?queueID=pipetka1023'   
+headers = {'user-agent': 'my-app-test/0.0.1'}
+r = requests.get(url, headers=headers)
+parsed_json = json.loads(r.text)
+for kill in parsed_json:
+    zkillLink="https://zkillboard.com/kill/"+str(kill.get("killmail_id"))+"/"
+    evkaLink="https://esi.evetech.net/v1/killmails/"+str(kill.get("killmail_id"))+"/"+ kill.get("zkb").get("hash")+"/"
+
+    totalVal= kill.get("zkb").get("totalValue")
+    #print(totalVal)
+    
+    locationID=kill.get("zkb").get("locationID")
+    
+    lokaStr=locDict.get(str(locationID),str(locationID))
+    
+    r1=requests.get(evkaLink, headers=headers)
+    parsed_json1 = json.loads(r1.text)
+    sysa=parsed_json1.get("solar_system_id")
+    timestamp=parsed_json1.get("killmail_time")
+    x=parsed_json1.get("victim").get("position").get("x")
+    y=parsed_json1.get("victim").get("position").get("y")
+    z=parsed_json1.get("victim").get("position").get("z")
+    ship=parsed_json1.get("victim").get("ship_type_id")
+    
+    dista=math.sqrt(x**2+y**2+z**2) /1000
+    dista=dista/149597870.7
+    
+    if typesDict.get(str(ship)) != 'Capsule':
+    #print("TotalVal "+str(totalVal) + " | ship "+ typesDict.get(str(ship))+ "  | sysa "    + systemDict.get(str(sysa)))
+        print("%25s TotalVal %14.2f   ship %-25s   sysa %20s    %17s    dist from sun %8.2f   ZKB %45s"
+              % (timestamp,totalVal, typesDict.get(str(ship)), systemDict.get(str(sysa)), lokaStr, dista, zkillLink))
